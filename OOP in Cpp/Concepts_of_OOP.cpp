7db57f931071f7e2517f899fd067a0d6ef0ad611
thishaniwijayaratne <anuradha.thishani@gmail.com>
2021-10-21 15:24:29 +0530

    Starting with inheritance, abstraction, and encapsulation.

--- OOP in Cpp/Concepts_of_OOP.cpp
+++ OOP in Cpp/Concepts_of_OOP.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
-from
+using namespace std;
+
+//  The Human Class ==========================================================
+class Human
+{
+public:
+    int age;
+
+protected:
+    int DoB;
+
+private:
+    long Uni_ID;
+};
+
+// The Students ==============================================================
+class Student
+{
+protected:
+    int age;
+};
+
+// The Employees Class =======================================================
+class Employee
+{
+private:
+    int age;
+};
+
+// Main Class
+int main()
+{
+    return 0;
+};
